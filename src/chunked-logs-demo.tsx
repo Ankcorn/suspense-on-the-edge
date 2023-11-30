5a8bfdb34df18055c531bd174d2e353ee1c67b94
Ankcorn <thomasankcorn@gmail.com>
2023-11-30 10:36:49 +0000

    add logs back

--- src/chunked-logs-demo.tsx
+++ src/chunked-logs-demo.tsx
@@ -1,11 +1,12 @@
 import { Hono } from 'hono'
 import { renderToReadableStream, Suspense } from 'hono/jsx/streaming'
 const app = new Hono()
-
+import { Buffer } from 'node:buffer';
 const AsyncComponent = async () => {
-  await new Promise((r) => setTimeout(r, 5000)) 
+  await new Promise((r) => setTimeout(r, 5000))
   return <div>Done!</div>
 }
+
 
 app.get('/', (c) => {
   const stream = renderToReadableStream(
@@ -18,8 +19,17 @@ app.get('/', (c) => {
       </body>
     </html>
   )
- 
-  return c.body(stream, {
+
+  const [res, log] = stream.tee();
+  const logStream = new TransformStream({
+    transform(chunk, controller) {
+      console.log(chunk.length)
+      console.log(Buffer.from(chunk).toString())
+      controller.enqueue(chunk)
+    }
+  })
+  log.pipeThrough(logStream)
+  return c.body(res, {
     headers: {
       'Content-Type': 'text/html; charset=UTF-8',
       'Transfer-Encoding': 'chunked',
